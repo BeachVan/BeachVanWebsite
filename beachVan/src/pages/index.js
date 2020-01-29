043b970c7dd483ee7fddc3eeb6a07c28d4c6a945
hankmitsch <hankm4@gmail.com>
2020-01-28 23:23:32 -0500

    install and begin Foundation config. Currently broken

--- beachVan/src/pages/index.js
+++ beachVan/src/pages/index.js
@@ -1,4 +1,7 @@
 import React from "react"
+@import '~foundation-sites/scss/util/util';
+@import './settings';
+@import '~foundation-sites/scss/foundation';
 
 export default () => <div><h1>Hello World</h1></div>
 
